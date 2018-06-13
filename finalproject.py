533d2c076bcf49b3b8df07b20ce994abe6a75fec
pradeepthi15pa1a04g9 <38848947+pradeepthi15pa1a04g9@users.noreply.github.com>
2018-06-14 00:30:29 +0530

    Update finalproject.py

--- finalproject.py
+++ finalproject.py
@@ -165,7 +165,7 @@ def getUserID(email):
         return None
 
 
-# DISCONNECT - Revoke a current user's token and reset their login_session
+# DISCONNECT - Revoke a current users token and reset their login_session
 @app.route('/gdisconnect')
 def gdisconnect():
     session = DBSession()
